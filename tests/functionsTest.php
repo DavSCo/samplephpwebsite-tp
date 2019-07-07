54541159396fc296b319a8fe64539fb1add53097
David Cohen <david.cohen@ynov.com>
2019-07-07 18:26:25 +0200

    test

--- tests/functionsTest.php
+++ tests/functionsTest.php
@@ -3,7 +3,7 @@ include(dirname(__FILE__).'/../functions.php');
 
 class FunctionsTest extends PHPUnit_Framework_TestCase
 {
-    public function siteNameIsOk() 
+    public function site_name_is_ok() 
     {
         siteName()->willReturn("Simple PHP Website");
     }
