616c842cb394fbe529a9abc9d1244e19612715e0
David Cohen <david.cohen@ynov.com>
2019-07-07 18:53:45 +0200

    test

--- tests/functionsTest.php
+++ tests/functionsTest.php
@@ -1,6 +1,6 @@
 <?php
 // include(dirname(__FILE__).'/../functions.php');
-namespace Tests\SimplePhpWebsite\Functions;
+namespace Tests\SimplePhpWebsite\Tests;
 
 use SimplePhpWebsite\Functions\Functions;
 
