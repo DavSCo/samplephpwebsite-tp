90657f08afd7ec7e280625e31daa2e7b359cafc3
David Cohen <david.cohen@ynov.com>
2019-07-07 18:40:26 +0200

    test

--- tests/functionsTest.php
+++ tests/functionsTest.php
@@ -1,7 +1,7 @@
 <?php
 // include(dirname(__FILE__).'/../functions.php');
 
-class FunctionTest extends PHPUnit_Framework_TestCase
+class FunctionsTest extends PHPUnit_Framework_TestCase
 {
     public function siteName_is_ok() 
     {
